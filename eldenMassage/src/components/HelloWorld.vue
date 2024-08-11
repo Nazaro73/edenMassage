d1507b3bd1dc8e90add922ddc1b7e285d86ed31a
khababaa <Abdel-Rahim.KHABABA@hotmail.com>
2024-08-11 02:15:46 +0200

    first commit

---
+++ eldenMassage/src/components/HelloWorld.vue
@@ -0,0 +1,372 @@
+
+
+<template>
+    <body>
+        <div>
+        <img class="img1" src="@/img/a23da41c-c6cd-401c-8740-5ee2a3489696.webp" alt="">
+        <div class="overlay">
+            <h1>L'Eden</h1>
+            <p>Venez vivre une expérience de bien-être unique ! </p>
+        </div>
+    </div>
+    
+    <div id="start">
+        <p>Faites quelque chose aujourd'hui dont votre futur vous remerciera. - Sean Patrick Flanery</p>
+    </div>
+    <div class="triangle"></div>
+    <section id ="concept">
+
+        <h2>Un Concepte issue de tradition ancestral</h2>
+    
+        <p>Découvrez le Head Spa, un concept unique inspiré de traditions ancestrales, conçu pour offrir une expérience de bien-être inégalée. <br>Ce soin de luxe commence par un nettoyage en profondeur et une hydratation minutieuse du cuir chevelu, suivi d’un massage spécifique qui non seulement détend mais stimule également la microcirculation sanguine. Cette stimulation aide à oxygéner les cellules et favorise une croissance saine des cheveux.
+
+            <br>Le rituel du Head Spa intègre une variété de techniques et de traitements qui travaillent en synergie pour libérer les tensions accumulées. Ce processus offre non seulement une relaxation profonde, mais également un bien-être global, transformant ce soin en une véritable échappée de la vie quotidienne. C’est une invitation à redécouvrir votre vitalité et à revitaliser votre esprit, tout en prenant soin de votre cuir chevelu et de vos cheveux.
+            
+           </p>
+
+    
+</section>
+    <section id="salon" >
+
+        <div>
+            
+            <h2> Un Refuge de Bien Etre en Plein Coeur de Chambery</h2>
+            <p>
+
+                Situé idéalement au cœur de Chambéry, le long de la paisible rive droite de la Leysse, l’Institut L’Éden est votre destination privilégiée pour une évasion hors du commun. Spécialisé dans le head spa, notre institut vous accueille dans un cadre serein où chaque soin est une expérience unique, méticuleusement adaptée à votre type de cheveux.
+                
+                À l’Institut L’Éden, nous nous engageons à vous offrir une déconnexion totale du tumulte quotidien. Nos soins, conçus pour revitaliser et soigner votre cuir chevelu, vous promettent une régénération complète et un ressourcement profond. Venez vous immerger dans une atmosphère de détente absolue et redécouvrir votre bien-être intérieur.
+                
+                 </p>
+        </div>
+        <img  class="img3" src="@/img/still-life-yoga-equipment.jpg" alt="Image du salon">
+        
+        
+    </section>
+
+    <section id="labels">
+        <h2>Nos produits sont :</h2>
+        <div >
+            <div id="label"><img src="@/img/100-Natural-PNG-Photo.png" alt="Produits Bio">
+                <p>100% naturels</p></div>
+            
+            <div id="label"><img src="@/img/logo-origine-france-garantie-OFG.png" alt="">
+                <p>100% fabriqué en france</p></div>
+            
+
+            <div id="label">
+                <img src="@/img/10004522.png" alt="">
+                <p>100% respectueux de l'environnement</p></div>
+            
+        </div>
+
+    </section>
+
+    <section id="offres">
+
+        <h2 >Les Offres Eden</h2>
+
+        <div class="massage-container">
+        <!-- Massage 1 -->
+        <div class="massage">
+            <img src="@/img/decouverte.jpg" alt="SOIN DÉCOUVERTE">
+            <div>
+                <h3>SOIN DÉCOUVERTE</h3>
+                <p>Offrez-vous un moment de détente absolue avec notre Soin Découverte. Ce soin comprend un massage relaxant conçu pour vous aider à évacuer le stress accumulé et favoriser une meilleure circulation sanguine. Laissez nos thérapeutes expérimentés manipuler et détendre vos muscles grâce à des techniques de massage éprouvées qui ciblent les zones de tension. Idéal pour se ressourcer et retrouver un équilibre corps-esprit, ce massage est parfait pour ceux qui cherchent à échapper à l'agitation quotidienne et à investir dans leur bien-être.</p>
+                <p class="price">45 MINUTES/60€</p>
+            </div>
+        </div>
+        <!-- Massage 2 -->
+        <div class="massage">
+            
+            <img src="@/img/premium.jpg" alt="SOIN PREMIUM">
+            <div>
+                <h3>SOIN PREMIUM</h3>
+                <p>Plongez dans une expérience de bien-être ultime avec notre Soin Premium, une séance luxueuse de 60 minutes conçue pour revitaliser et rajeunir. Ce soin complet commence par une analyse détaillée suivie d’un massage et d'un gommage exquis du cuir chevelu, vous plongeant ainsi dans un état de détente profonde dès les premiers instants.<br><br> Nous utilisons des techniques spécialisées de shampooing et d’application de masque Rituel pour nourrir vos cheveux et votre cuir chevelu. La session est complétée par un soin du visage personnalisé, où vous avez le choix entre un Jelly Masque hydratant ou un Masque Tissus, chacun adapté à vos besoins spécifiques de la peau, assurant une éclatante fraîcheur. Ce traitement est idéal pour ceux qui souhaitent s’offrir un moment de luxe et d'évasion, laissant votre peau, vos cheveux et votre esprit parfaitement soignés et rafraîchis.</p>
+                <p class="price">60 MINUTES/89€</p>
+        </div>
+        </div>
+        <!-- Massage 3 -->
+        <div class="massage">
+            <img src="@/img/sds.jpg" alt="SOIN « un temps pour Soie »">
+            <div>
+            <h3>SOIN « un temps pour Soie »</h3>
+            <p>Offrez-vous une échappée belle de pure détente avec notre Soin « Un Temps pour Soie », un traitement de 90 minutes qui allie luxe et soin personnalisé. Ce soin débute par une analyse approfondie de votre cuir chevelu, suivi d’un massage et d’un gommage enrichis en huile essentielle, revitalisant votre cuir chevelu tout en diffusant des arômes apaisants. Le traitement se poursuit avec un shampooing et un masque à la protéine de soie, restaurateur de l'éclat et de la souplesse de vos cheveux.
+
+                <br><br>La séance est complétée par un rituel visage incluant un massage doux et un masque à la protéine de soie, disponible en version Jelly pour une hydratation profonde ou en tissu pour une action réparatrice intensive. Ce soin est spécialement conçu pour nourrir et régénérer, laissant votre peau, vos cheveux et votre esprit dans un état de bien-être sublime.</p>
+            <p class="price">90 minutes/129 €</p>
+            </div>
+        </div>
+    </div>
+
+</section>
+
+    <section id="reservation">
+        <h2>Réservation</h2>
+        <p>Veuillez utiliser le calendrier ci-dessous pour réserver votre créneau.</p>
+        <div class="googleagenda"><iframe class="calendar" src="https://calendar.google.com/calendar/u/0/appointments/schedules/AcZssZ0-PN2oP1PTS-lLae2yXcV5an6SGY8L1AtK9lAyi6QKTt3sDM8LazeV0vdJA7J0YH5EzIOE4eeO" style="border: 0" width="800" height="800" frameborder="0" scrolling="no"></iframe></div>
+    </section>
+
+    <footer>
+        <div class="contact-info">
+            <p><strong>Institut :</strong></p>
+            <div>22 rue Paulette Besson, 73000 Chambéry</div>
+            <p><strong>Nos Horaires :</strong></p>
+            <div>Lundi au Vendredi 9h30-18h30</div>
+            <div>Samedi 9h30-14h00</div>
+            <div>Mercredi fermé</div>
+            <p><strong>Sandrine :</strong> 07 50 44 76 98</p>
+            <p>contact@maia-massages.fr</p>
+            <p><strong>UNIQUEMENT SUR RDV</strong></p>
+        </div>
+        <div class="social-media">
+            <p>Suivez-nous sur les réseaux sociaux !</p>
+            <a href="#">Facebook</a>
+            <a href="#">Instagram</a>
+            <a href="#">Twitter</a>
+        </div>
+    </footer>
+    </body>
+  
+</template>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Satisfy&display=swap');
+
+@import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Satisfy&display=swap');
+
+@import url('https://fonts.googleapis.com/css2?family=Lora:wght@400;700&display=swap');
+/* Réinitialisation de quelques styles par défaut et typographie générale */
+body {
+    font-family: 'Lora', serif;
+    background-color:#ebe6de;
+    color: #4a4a4a;
+    margin: 0;
+    padding: 0px;
+}
+
+p {
+    font-size: 1.1em;
+    line-height: 1.6;
+}
+
+h2 {
+    text-align: center;
+    font-size:3em;
+    font-family: Satisfy, cursive;
+}
+
+#start {
+    background-color: #e3d7c5;
+    height: 60vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    
+
+}
+
+#start p {
+    width: 40%;
+    font-family: 'Satisfy', cursive;
+    font-size: 2.5em;
+}
+
+.triangle {
+    width: 0%;
+    height: 0%;
+    border-left: 50vw solid transparent;/* 50% width */
+    border-right: 50vw solid transparent;/* 50% width */
+    border-top: 7vw solid #e3d7c5; /* Adjust the color to match your triangle */
+}
+
+
+#concept p {
+    width: 60%;
+    margin: 0 auto;
+    
+
+}
+
+section {
+    padding: 10vw;
+}
+
+
+
+/* Style spécifique pour la section réservation */
+#reservation h2, #reservation p {
+    text-align: center;
+    margin-top: 20px;
+}
+
+#reservation {
+    padding-top:5vw ;
+    background-color: #c6a68e;
+}
+
+
+#salon {
+    display: flex; /* Utilise Flexbox pour aligner les éléments */
+    justify-content: space-around; /* Centre les éléments horizontalement */
+    align-items: center; /* Centre les éléments verticalement */
+    background-color: #e3d7c5;
+    
+}
+
+.img3 {
+    width: 30%; /* Rend l'image un peu plus grande que le texte */
+    height: auto; /* Garde les proportions de l'image */
+    z-index: 1; /* S'assure que l'image est derrière le texte */
+}
+
+#salon > div {
+    
+    padding: 40px; /* Ajoute de l'espace autour du texte */
+     
+    width: 40%; /* Définit une largeur pour le conteneur de texte */
+    height: 100%;
+    text-align: center; /* Centre le texte à l'intérieur du conteneur */
+}
+
+/* Style de l'image en pleine largeur */
+.img1 {
+    width: 100%;
+    height: 100vh; /* Hauteur de l'écran */
+    display: block;
+    position: relative; /* Nécessaire pour la position de la superposition */
+}
+
+#labels {
+    background-color: #c6a68e;
+    padding: 3vw;
+}
+
+#labels div {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+
+
+}
+
+#labels div p {
+    font-size: 1.7em;
+    font-family: Satisfy, cursive; 
+}
+
+#labels div img{
+    width: 15vw;
+    height: 15vw;
+}
+
+
+#label{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 30%;
+
+}
+
+/* Style de la superposition */
+.overlay {
+    position: absolute; /* Positionnement par rapport à l'image */
+    top: 50%; /* Centrage vertical */
+    left: 30%; /* Décale l'overlay pour qu'il soit centré dans la moitié gauche */
+    transform: translateY(-50%); /* Centrage parfait verticalement */
+    width: 35%; /* Réduction de la largeur pour que l'overlay soit plus petit */
+    height: 60%; /* Ajuster la hauteur selon le besoin */
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    background: rgba(204, 192, 127, 0); /* Blanc semi-transparent */
+    padding: 0 2%; /* Petit padding pour ne pas coller au bord */
+    border-radius: 20px; /* Coins arrondis */
+}
+
+.overlay h1, .overlay p {
+    color: #e3d7c5; /* Couleur du texte, ajustez selon votre design */
+    text-align: center;
+    width: 100%; /* Permet au texte de prendre toute la largeur de .overlay */
+}
+
+/* Ajustements pour le texte */
+.overlay h1 {
+    margin-bottom: 20px; /* Espace entre le h1 et le paragraphe */
+    font-size: 4rem; /* Taille du titre ajustable selon les besoins */
+}
+
+.overlay p {
+    font-size: 1.7rem; /* Taille du texte ajustable selon les besoins */
+}
+
+
+.massage-container {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-around;
+    gap: 20px;
+    margin-top: 20px;
+}
+
+
+
+.googleagenda {
+    display: flex;
+    justify-content: center;
+    
+}
+
+.massage {
+    display: flex;
+    align-items: center;
+    padding: 5vw;
+}
+
+.massage p {
+    width: 30vw;
+    font-size: 1.2em;
+}
+.massage img {
+    width: 25vw;
+    margin-right: 5vw;
+    
+}
+
+.massage h3{
+    font-size: 2em;
+    font-family: Satisfy, cursive;
+}
+
+
+
+
+.img1 {
+    filter: brightness(30%);
+  }
+
+footer {
+    background-color: #e3d7c5; /* Couleur du texte, ajustez selon votre design */
+    padding: 20px;
+    text-align: center;
+    font-family: Arial, sans-serif;
+    border-top: 1px solid #e7e7e7;
+
+}
+.contact-info {
+    margin-bottom: 20px;
+}
+.contact-info p, .contact-info div {
+    margin: 5px 0;
+}
+.social-media {
+    margin-top: 20px;
+}
+.social-media a {
+    margin: 0 10px;
+    text-decoration: none;
+    color: #555;
+}
+</style>
